c63eef89ff348e2b9dc7113d856bb520e8ed7ae7
txqqe <FSOLY115@hotmail.com>
2024-10-28 00:22:38 +0300

    LibraryTest.java

--- Main - Copy/src/LibraryTest.java
+++ Main - Copy/src/LibraryTest.java
@@ -27,6 +27,20 @@ class book{
         this.isbn = isbn;
     }
 }
+public class ebook extends book {
+    double fileSizeMB;
+    public ebook(String title, String author, String isbn, double fileSizeMB){
+        super(title, author, isbn);
+        /////////////////////
+        this.fileSizeMB = fileSizeMB;
+    }
+    public void displayInfo(){
+        System.out.println(gettitle());
+        System.out.println(getauthor());
+        System.out.println(getisbn());
+         System.out.println(this.fileSizeMB);
+    }
+}
 
 
 
