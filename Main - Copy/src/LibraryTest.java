4100d11eb3a3a542f7bd990e9d1f9003e1663da0
txqqe <FSOLY115@hotmail.com>
2024-10-28 00:23:55 +0300

    Update LibraryTest.java

--- Main - Copy/src/LibraryTest.java
+++ Main - Copy/src/LibraryTest.java
@@ -42,6 +42,23 @@ public class ebook extends book {
     }
 }
 
+public class AudioBook extends book {
+   int durationMinutes; 
+    public AudioBook(String title, String author, String isbn, int durationMinutes){
+        super(title, author, isbn);
+        ////////////////////////
+        this.durationMinutes = durationMinutes;
+    }
+    public void displayInfo(){
+        System.out.println(gettitle());
+        System.out.println(getauthor());
+        System.out.println(getisbn());
+        System.out.println(this.durationMinutes);
+    }
+
+    
+}
+
 
 
 
